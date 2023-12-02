dcb927d9aa6e056738f969a12d11bc69d802b78e
Alonso-Dominguez <122809477+Alonso-Dominguez@users.noreply.github.com>
2023-12-01 19:48:17 -0600

    Update listar.js

--- static/js/listar.js
+++ static/js/listar.js
@@ -1,19 +1,24 @@
 async function cargarContactos() {
     const apiURL = 'https://backend-contactos-jadc-b1444a39f4f7.herokuapp.com/contactos';
-    const response = await fetch(apiURL, {method: 'GET', mode: 'cors'});
-    const data = await response.json();
-  
-    if (response.status === 200) {
-        const contactos = await response.json();
-        const listaContactos = document.getElementById('lista-contactos');
 
-        for (const contacto of contactos) {
-            const listItem = document.createElement('li');
-            listItem.textContent = `${contacto.id_contacto} ${contacto.nombre} ${contacto.primer_apellido} ${contacto.segundo_apellido} - Email: ${contacto.email} - Teléfono: ${contacto.telefono}`;
-            listaContactos.appendChild(listItem);
+    try {
+        const response = await fetch(apiURL, { method: 'GET', mode: 'cors' });
+
+        if (response.status === 200) {
+            const contactos = await response.json();
+            const listaContactos = document.getElementById('lista-contactos');
+
+            for (const contacto of contactos) {
+                const listItem = document.createElement('li');
+                listItem.textContent = `${contacto.id_contacto} ${contacto.nombre} ${contacto.primer_apellido} ${contacto.segundo_apellido} - Email: ${contacto.email} - Teléfono: ${contacto.telefono}`;
+                listaContactos.appendChild(listItem);
+            }
+        } else {
+            alert('Error al cargar la lista de contactos.');
         }
-    } else {
-        alert('Error al cargar la lista de contactos.');
+    } catch (error) {
+        console.error('Error de red:', error);
+        alert('Error de red al cargar la lista de contactos.');
     }
 }
 
